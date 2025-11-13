6af80ddb00de808738a1f98d42a304bc00f555a7
Abhishek Namdev <namdevabhi025@gmail.com>
2025-11-13 22:29:24 +0530

    Refactor CORS middleware configuration

--- Portfolio/server/index.js
+++ Portfolio/server/index.js
@@ -1,3 +1,20 @@
+// import express from "express";
+// import cors from "cors";
+// import dotenv from "dotenv";
+// import mailRoutes from "./backend/routers/mailRouter.js";
+
+// dotenv.config();
+
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
+// app.use("/api", mailRoutes);
+
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
+
+
+
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -6,9 +23,21 @@ import mailRoutes from "./backend/routers/mailRouter.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+
+app.use(
+  cors({
+    origin: ["https://portfolio-amgn.onrender.com"],
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 app.use(express.json());
 app.use("/api", mailRoutes);
 
+app.get("/", (req, res) => {
+  res.send("Backend is running...");
+});
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
