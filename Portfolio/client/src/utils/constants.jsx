30a44121a02937900692603a644416db3079bd6b
Abhishek Namdev <namdevabhi025@gmail.com>
2025-11-13 21:50:52 +0530

    Update BASE_URL to new Vercel deployment

--- Portfolio/client/src/utils/constants.jsx
+++ Portfolio/client/src/utils/constants.jsx
@@ -1,1 +1,5 @@
-export const BASE_URL = "https://portfolio-backend-i3sr.onrender.com";
+// export const BASE_URL = "https://portfolio-backend-i3sr.onrender.com";
+
+
+
+export const BASE_URL = "https://portfolio-b-end.vercel.app/";
